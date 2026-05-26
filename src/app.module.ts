d581a23cbfd7b26b66828613f74dfea3fccf1fed
Gabrieelgc2 <gabrielao8@hotmail.com>
2026-05-26 09:59:28 -0300

    add: google sign in jwt database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FirebaseModule } from './firebase/firebase.module';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 import { OportunidadesController } from './oportunidades/oportunidades.controller';
 
 @Module({
-  imports: [FirebaseModule],
+  imports: [
+    PrismaModule,
+    FirebaseModule,
+    AuthModule,
+  ],
   controllers: [AppController, OportunidadesController],
   providers: [AppService],
 })
