e04ba75eaca1926c3f8522dd2f0fc84f343eaf95
Gabrieelgc2 <gabrielao8@hotmail.com>
2026-06-17 11:49:11 -0300

    fix: removed api/index.ts and removed library express

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,17 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
-import { ExpressAdapter } from '@nestjs/platform-express'
-
-import serverlessExpress from '@codegenie/serverless-express'
-import express from 'express'
 
 import { AppModule } from './app.module'
 
-const expressApp = express()
-let server: any
 let appInitialized = false
 
 async function createNestApp() {
   if (appInitialized) {
-    return
+    return  // Se já foi inicializado, não inicializa novamente
   }
 
   const app = await NestFactory.create(
     AppModule,
-    new ExpressAdapter(expressApp)
   )
 
   const allowedOrigins = process.env.ALLOWED_ORIGINS
@@ -27,7 +20,7 @@ async function createNestApp() {
 
   app.enableCors({
     origin: (origin, callback) => {
-      // Permitir requisições sem origem (como apps mobile, curl ou requests server-to-server)
+      // Permitir requisições sem origem (como Webhooks, curl, Postman, etc.)
       if (!origin) return callback(null, true)
 
       if (process.env.NODE_ENV === 'production') {
@@ -60,21 +53,10 @@ async function createNestApp() {
 async function bootstrapLocal() {
   await createNestApp()
   const port = parseInt(process.env.PORT || '3000', 10)
-  await expressApp.listen(port)
   console.log(`Application is running on: http://localhost:${port}`)
 }
 
-async function getServerlessHandler() {
-  await createNestApp()
-  server = server ?? serverlessExpress({ app: expressApp })
-  return server
-}
 
 if (require.main === module) {
   void bootstrapLocal()
-}
-
-export default async function handler(req: any, res: any) {
-  const srv = await getServerlessHandler()
-  return srv(req, res)
 }
